e0555f367d7be9a6a8743ba9f16693e94f6bb915
Florian Kuc <florian.kuc@gmail.com>
2020-04-02 16:03:58 +0200

    fix more

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,7 +27,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
             module: {
                 rules: [
                     {
-                        test: /node_modules\/scrollmagic\/scrollmagic/,
+                        test: /node_modules\/scrollmagic\/scrollmagic\/uncompressed/,
                         use: loaders.null(),
                     },
                 ],
